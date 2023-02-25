81e4087cefd1fccd71edea18f6afce37136d8bfd
arduinocc04 <arduinocc04@gmail.com>
2023-02-25 19:48:13 +0900

    use vm

--- function_app.py
+++ function_app.py
@@ -1,18 +1,7 @@
-import typing
-import logging
-
-import azure.functions as func
-
 import Crawling
 import Slack
 
-app = func.FunctionApp()
-
-@app.function_name(name="noticeNotifierTimer")
-@app.schedule(schedule="0 */30 * * * *", arg_name="noticeNotifierTimer", run_on_startup=False,
-              use_monitor=False) 
-def noticeNotifier(noticeNotifierTimer:func.TimerRequest) -> None:
-    logging.info("noticeNotifier called.")
+def noticeNotifier() -> None:
     Bachelor = Crawling.Sogang(Crawling.BACHELOR_NOTICE_LINK, Crawling.PEM_FILE_LOCATION, Crawling.HEADERS, "NoticeUpdateData/bachelor.txt", "<학사공지>")
     Scholar = Crawling.Sogang(Crawling.SCHOLARSHIP_NOTICE_LINK, Crawling.PEM_FILE_LOCATION, Crawling.HEADERS, "NoticeUpdateData/scholarship.txt", "<장학공지>")
     General = Crawling.Sogang(Crawling.GENERAL_SUPPORT_NOTICE_LINK, Crawling.PEM_FILE_LOCATION, Crawling.HEADERS, "NoticeUpdateData/generalSupport.txt", "<일반지원>")
@@ -35,14 +24,6 @@ def noticeNotifier(noticeNotifierTimer:func.TimerRequest) -> None:
             for title, url in mostRecentNotices:
                 Slack.sendTextWithLink(title, url)
             p.runAll()
-            logging.info(f"{p.name} updated. slack message was sent.")
 
-@app.function_name(name="tester")
-@app.route(route="test", auth_level=func.AuthLevel.ANONYMOUS)
-def tester(req:func.HttpRequest) -> func.HttpResponse:
-    Bachelor = Crawling.Sogang(Crawling.BACHELOR_NOTICE_LINK, Crawling.PEM_FILE_LOCATION, Crawling.HEADERS, "NoticeUpdateData/bachelor.txt", "학사공지")
-    logging.info("test.")
-    Bachelor.getSoup()
-    tmp = Bachelor.getPostCount()
-    Slack.sendTextWithLink(f'Test: {tmp}', "https://test.com")
-    return func.HttpResponse(str(tmp))
+if __name__ == "__main__":
+    noticeNotifier()
